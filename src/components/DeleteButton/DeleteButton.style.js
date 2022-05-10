82a55e276fc97b3934ca7d5503f577949d0c2354
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-05-10 03:36:51 +0200

    Added style to the DeleteButton component

---
+++ src/components/DeleteButton/DeleteButton.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const DeleteButton = styled.button`
+  font-size: 35px;
+  color: #d179cd;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`;
+
+export default DeleteButton;
