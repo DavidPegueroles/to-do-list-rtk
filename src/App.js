524d1b6edde3d515dd1d6730c9f73fb325cd515d
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-05-10 04:04:21 +0200

    Added renderisation of toDos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Section, Title } from "./App.style";
+import Form from "./components/Form/Form";
+import Task from "./components/Task/Task";
+import toDosData from "./data/toDosData";
+import { loadToDosActionCreator } from "./redux/features/toDosSlice";
+
 const App = () => {
+  const toDos = useSelector((state) => state.toDos);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadToDosActionCreator(toDosData));
+  }, [dispatch]);
+
   return (
     <>
-      <h1>To Do List</h1>
+      <Title>To Do List</Title>
+
+      <Section>
+        <Form />
+      </Section>
+
+      <Section>
+        <ul>
+          {toDos.map(({ id, name, done }) => (
+            <li key={id}>
+              <Task id={id} name={name} done={done}></Task>
+            </li>
+          ))}
+        </ul>
+      </Section>
     </>
   );
 };
